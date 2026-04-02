79d8eacad35c5c0bd6b6da95a1c4d16e3f724a18
Juan-Xavier375 <juanxavier735@gmail.com>
2026-04-01 21:18:34 -0300

    feat: render tasks to the DOM

--- main.js
+++ main.js
@@ -1,12 +1,22 @@
 const addTaskBtn = document.getElementById('add-task-btn');
 const addTaskInput = document.getElementById('task-input');
+const tasks = document.getElementById('task-list');
+
+function addLi() {
+  return document.createElement('li');
+}
+
+function addTaskToList(inputText) {
+  const li = addLi();
+  li.innerText = inputText;
+  tasks.appendChild(li);
+}
 
 addTaskBtn.addEventListener('click', function () {
-  const taskText = addTaskInput.value;
-
+  const taskText = addTaskInput.value.trim();
   if (taskText === '') return;
 
-  console.log(taskText);
+  addTaskToList(taskText);
 
   addTaskInput.value = '';
 });
